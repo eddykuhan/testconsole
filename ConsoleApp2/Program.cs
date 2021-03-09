a321c00fe1fa385312ce9d084e99f42ddc583c58
kdhanyan <kuhan.dhanyan@intel.com>
2021-03-09 23:16:44 +0800

    added func

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,20 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
- public   class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             //class behaviour changes at runtime 
-            var str = new StrategySetup(new OperationAdd());
 
+            var str = new StrategySetup(GetStrategy);
             Console.WriteLine(str.executeStreategy(1, 2));
+
             Console.ReadLine();
         }
+        private static Func<OperationEnum, IStrategy> GetStrategy = key =>
+        {
+            {
+
+                switch (key)
+                {
+                    case OperationEnum.Add:
+                        return new OperationAdd();
+                    case OperationEnum.Multiply:
+                        return new OperationMultiply();
+                    default:
+                        break;
+
+                }
+                return null;
+            }
+        };
+
     }
+
 }
+
+public enum OperationEnum
+{
+    Add,
+    Multiply
+}
+
--- ConsoleApp2/StrategySetup.cs
+++ ConsoleApp2/StrategySetup.cs
@@ -1,18 +1,21 @@
-﻿namespace ConsoleApp2
+﻿
+using System;
+
+namespace ConsoleApp2
 {
     public class StrategySetup
     {
-        private IStrategy strategy;
-        public StrategySetup(IStrategy strategy)
-        {
-            this.strategy = strategy;
-        }
+        private readonly Func<OperationEnum, IStrategy> func;
 
+        public StrategySetup(Func<OperationEnum, IStrategy> func)
+        {
+            this.func = func;
+        }
         public int executeStreategy(int num, int num2)
         {
-
-            return strategy.doOperation(num, num2);
+            return func(OperationEnum.Add).doOperation(num, num2);
 
         }
+
     }
 }
